5e56c977e497848c063378bc7e193056b30d9c7a
nityaboyapati99 <113191142+nityaboyapati99@users.noreply.github.com>
2023-04-03 07:03:28 +0530

    Create AtmOperationIntf.java

---
+++ AtmOperationIntf.java
@@ -0,0 +1,11 @@
+package com.sjprogramming;
+
+public interface AtmOperationInterf {
+    public void viewBalance();
+
+    public void withdrawAmount(double withdrawAmount);
+
+    public void depositAmount(double depositAmount);
+
+    public void viewMiniStatement();
+}
